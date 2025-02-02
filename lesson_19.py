4a889534a85700323a83758fdf79b7d3db9be214
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-02-02 17:14:51 +0500

    lesson_19: декоратор и проблема в количестве аргументов для wrapper

--- lesson_19.py
+++ lesson_19.py
@@ -72,5 +72,29 @@ def get_sum(num_list: List[int]) -> int:
 print(multiply(5, 6))
 print(multiply(5, "6"))
 
-data_set = [1, 2, 3, 4, 5, 6, "7", 8, 9, 10]
+data_set = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(get_sum(data_set))
+
+################# Декораторы #################
+
+def simple_decorator2(func: Callable):
+    # func, message
+    def wrapper(a: int, b: int):
+        print(f'Печать до вызова.')
+        result = func(a, b)
+        print(f'Печать после вызова.')
+
+        return result
+    
+    return wrapper
+
+@simple_decorator2
+def multiply2(a: int, b: int) -> int:
+    return a * b
+
+@simple_decorator2
+def multiply3(a: int, b: int, c: int) -> int:
+    return a * b * c
+
+multiply2(5, 6)
+multiply3(5, 6, 7)
