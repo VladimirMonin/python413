d5deda177a54bae0020981e98b0ca8bbf45c1928
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-02-08 15:34:42 +0500

    lesson_20: map функция высшего порядка

---
+++ lesson_20.py
@@ -0,0 +1,27 @@
+"""
+08.02.2025
+Тема: Функции Ч7. Анонимные функции. Map Filter Sorted. Урок: 20
+- Функции высшего порядка
+"""
+from typing import Callable, Iterable, Union, Any, List, Tuple, Set
+# Функция может принемать функцию
+
+def my_map(func: Callable, data: Union[List, Tuple, Set]) -> List[Any]:
+    result = []
+
+    for item in data:
+        result.append(func(item))
+    
+    return result
+
+potatos = ["картошка", "картошка", "гнилая_картошка", "картошка", "гнилая_ картошка", "картошка"]
+
+def clean_potato(potato: str) -> str:
+    return potato + "_очищенная"
+
+cleaned_potatos = my_map(clean_potato, potatos)
+print(cleaned_potatos)
+
+# используем встроеный map
+cleaned_potatos_2 = map(clean_potato, potatos) # list( <map object at 0x00000276552F9B10> )
+print(cleaned_potatos_2)
