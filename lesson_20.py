2ff039080f8291960586d8e19740c0f521701438
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-02-08 17:36:22 +0500

    lesson_20: filter и метод списка sort()

--- lesson_20.py
+++ lesson_20.py
@@ -2,61 +2,104 @@
 08.02.2025
 Тема: Функции Ч7. Анонимные функции. Map Filter Sorted. Урок: 20
 - Функции высшего порядка
+- Написали собственный my_map - чтобы разобрать как работает map
+- Написали собственный my_filter - чтобы разобрать как работает filter
+- Познакомились с lambda
+- Посмотрели как работает lambda с фильтр
 """
-from typing import Callable, Iterable, Union, Any, List, Tuple, Set
-# Функция может принемать функцию
+# 1. Список чисел от пользователя
+# 1.1 Цикл
 
-def my_map(func: Callable, data: Union[List, Tuple, Set]) -> List[Any]:
-    result = []
+# users_nums = input('Введите числа через пробел: ').split()
 
-    for item in data:
-        result.append(func(item))
-    
-    return result
+# nums_list = []
 
-potatos = ["картошка", "картошка", "гнилая_картошка", "картошка", "гнилая_ картошка", "картошка"]
+# for num in users_nums:
+#     nums_list.append(int(num))
 
-def clean_potato(potato: str) -> str:
-    return potato + "_очищенная"
 
-cleaned_potatos = my_map(clean_potato, potatos)
-print(cleaned_potatos)
+# 1.2 Списковое выражение
+# nums_list = [int(num) for num in input('Введите числа через пробел: ').split()]
 
-# используем встроеный map
-cleaned_potatos_2 = list(map(clean_potato, potatos)) # list( <map object at 0x00000276552F9B10> )
-print(cleaned_potatos_2)
+# 1.3 Map к списку строк
+# users_nums = input('Введите числа через пробел: ').split()
+# users_nums = list(map(int, users_nums))
 
-# ФИЛЬТР
+# users_nums = list(map(lambda num: int(num) if num.isdigit() else None, users_nums))
 
-def my_filtr(func: Callable, data: Iterable) -> List:
-    result = []
+from data.marvel import full_dict
 
-    for item in data:
-        if func(item):
-            result.append(item)
-    
-    return result
+full_list = [{"id": film_id, **film} for film_id, film in full_dict.items()]
 
-def is_bad_wegetable(wegetable: str) -> bool:
-    return "гнил" in wegetable.lower()
+# 2. Фильмы первой фазы
+stage = "Первая фаза"
 
-bad_potatos = my_filtr(is_bad_wegetable, potatos)
-print(bad_potatos)
+result = []
 
-# используем встроеный filter
-bad_potatos_2 = list(filter(is_bad_wegetable, potatos)) # <filter object at 0x000001B43843B550>
-print(bad_potatos_2)
+for film in full_list:
+    if film['stage'] == stage:
+        result.append(film)
 
-# LAMBDA
+# 2.1 Списковое выражение
+first_stage = [film for film in full_list if film['stage'] == stage]
 
-def multiple(a, b):
-    return a * b
+# 2.2 Filter
+first_stage = list(filter(lambda film: film['stage'] == stage, full_list))
 
-anonimus = lambda a, b: a * b
-print(multiple(2, 2))
-print(anonimus(2, 2))
+# 3. Фильмы 2018 года
+films_2018 = [film for film in full_list if film['year'] == 2018]
+films_2018 = list(filter(lambda film: film['year'] == 2018, full_list))
 
-potatos = ["картошка", "картошка", "гнилая_картошка", "картошка", "гнилая_ картошка", "картошка"]
+# 4. Фильмы после 2020 года
+# films_after_2020 = [film for film in full_list if film['year'] > 2020]
+# films_after_2020 = list(filter(lambda film: film['year'] > 2020, full_list))
+# TypeError: '>' not supported between instances of 'str' and 'int'
 
-good_potatos = list(filter(lambda potato: "гнил" not in potato.lower(), potatos))
-print(good_potatos)
+# Нам надо проверить является ли это числом перед сравнением
+films_after_2020 = [film for film in full_list if isinstance(film["year"], int) and film['year'] > 2020] 
+films_after_2020 = list(filter(lambda film: isinstance(film["year"], int) and film['year'] > 2020, full_list))
+
+from pprint import pprint
+# pprint(films_after_2020)
+
+# СОРТИРОВКА!
+# list.sort() - метод списков
+# key - необязательный параметр для сортировки ()
+# reverse - необязательный параметр для сортировки по убыванию
+
+participants = [
+    "Владимир",
+    "Олег",
+    "Кирилл",
+    "Алексей",
+    "Александр",
+    "Андрей",
+    "Ильдар",
+    "Андрей",
+    "Никита",
+    "Даши",
+    "Сергей",
+    "Максим",
+    "Никита",
+    "Анна",
+    "Егор",
+    "Елена"
+]
+
+participants.sort(key= lambda name: name[-1])
+print(participants)
+
+# 5. Сортировка full list по году и по названию
+
+def film_sorter(film):
+    title = film['title']
+    year = film['year']
+
+    title = title if title else "Без названия"
+    year = year if isinstance(year, int) else 0
+    return year, title
+
+full_list.sort(key=film_sorter)
+pprint(full_list)
+
+full_list.sort(key=lambda film: (film['year'] if isinstance(film['year'], int) else 0, film['title'] if film['title'] else "Без названия"))
