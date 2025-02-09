86d521b6a0c2059dcb2ba2cfb66c908cc50039e2
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-02-09 16:23:35 +0500

    lesson_21: цепочка генераторов из range - filter - map

--- lesson_21.py
+++ lesson_21.py
@@ -7,43 +7,13 @@ from typing import Generator
 START_VALUE = 0
 END_VALUE = 1_000_000_000_000_000_000
 
-# list_range = list(range(START_VALUE, END_VALUE))
+range_obj = range(START_VALUE, END_VALUE)
 
-# range_object = range(START_VALUE, END_VALUE)
-# print(range_object, type(range_object))
+# Помещаем это в фильтр для получения только четных чисел. Получаем объект фильтра
+even_numbers = filter(lambda x: x % 2 == 0, range_obj)
 
-# for num in range_object:
-#     print(num)
+# Получаем map объект из even_numbers и превращаем числа в строки и добавляем число
+even_numbers_str = map(lambda x: f"Число: {x}", even_numbers)
 
-potatos = ["картошка", "картошка", "гнилая картошка", "картошка"]
-
-def clean_potatos(potatos: list[str]) -> list[str]:
-    new_potatos = []
-    
-    for potato in potatos:
-        potato += "чищенная"
-        new_potatos.append(potato)
-    return new_potatos
-
-
-cleaned_potatos = clean_potatos(potatos)
-print(cleaned_potatos)
-
-# Генераторные функции. Yield.
-def generator_clean_potatos(potatos: list[str]) -> Generator[str]:
-    for potato in potatos:
-        potato += "_чищенная"
-        yield potato
-
-cleaned_potatos = generator_clean_potatos(potatos)
-print(next(cleaned_potatos))
-print(next(cleaned_potatos))
-print(next(cleaned_potatos))
-# StopIteration
-
-
-for potato in cleaned_potatos:
-    print(potato, "Из цикла")
-
-for potato in generator_clean_potatos(potatos):
-    print(potato, "Из цикла_2")
+for num in even_numbers_str:
+    print(num)
