784e6c4440d7e46bb3c7fb2466a97f71ad1f4064
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-02-15 15:36:32 +0500

    lesson_22: синтаксис классов и первый класс Person

---
+++ lesson_22.py
@@ -0,0 +1,18 @@
+"""
+15.02.2025
+Python: ООП. Ч1. Атрибуты и методы. Класс и экземпляр. Практика. Урок: 22
+- class
+- нейминг классов UpperCamelCase
+"""
+
+class Person:
+    pass
+
+p1 = Person()
+p2 = Person()
+
+print(p1, p2)
+print(type(p1), type(p2))
+# <__main__.Person object at 0x000001E973C16F90> 
+# <__main__.Person object at 0x000001E973EA8CD0>
+# <class '__main__.Person'> <class '__main__.Person'>
