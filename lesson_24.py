5bd9e8e0e8a22c52c0461199729e0db919ccf9b3
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-03-01 15:56:29 +0500

    lesson_24: пример с приватным методом валидации и классами Car и Driver

--- lesson_24.py
+++ lesson_24.py
@@ -14,13 +14,41 @@ class Car:
 
     def __str__(self):
         return f"Марка {self.model}, максимальная скорость {self.__max_speed}"
+    
+    def __validate_speed(self, speed: int):
+        """
+        Приватный метод-валидатор скорости.
+        Проверяет, что скорость не отрицательная и не превышает максимальную.
+        """
+        if speed < 0:
+            raise ValueError("Скорость не может быть отрицательной")
+        
+        if speed > self.__max_speed:
+            raise ValueError("Скорость не может быть больше максимальной")
+    
+    
+    def set_speed(self, speed: int):
+        """
+        Публичный метод для установки скорости.
+        Вызывает приватный метод-валидатор.
+        """
+        self.__validate_speed(speed)
+        self._speed = speed
+
+
+class Driver:
+    def __init__(self, name: str, car: Car):
+        self.name = name
+        self.car = car
+
+    def drive(self, speed: int):
+        print(f"{self.name} сел в {self.car.model}")
+        self.car.set_speed(speed)
+        print(f"Скорость {self.car.model}: {self.car._speed}")
+
 
 volga = Car("Volga", "black")
-print(volga.model)
-print(volga.color)
-print(volga._speed)
-# print(volga.__max_speed)
-print(volga._Car__max_speed)
-
-# AttributeError: 'Car' object has no attribute '__max_speed'. Did you mean: '_Car__max_speed'?
-# Шаблон искажения имени _<ClassName>__<attributeName>
+driver = Driver("Иосиф", volga)
+driver.drive(100)
+driver.drive(200)
+driver.drive(300)
