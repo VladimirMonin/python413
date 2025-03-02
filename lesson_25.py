20a62d1e7d2e7997ab0dff3bc0511186b5c472f2
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-03-02 17:06:12 +0500

    lesson_25: класс MistralAiImageChat для работы с изображениями

--- lesson_25.py
+++ lesson_25.py
@@ -4,8 +4,10 @@
 """
 
 from dotenv import load_dotenv
+
 # pip install python-dotenv
 import os
+import base64
 
 load_dotenv()
 
@@ -14,17 +16,16 @@ MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
 # Тут мы можем вылавливать из переменных окружения ключи к API
 
 from mistralai import Mistral
-model = "mistral-large-latest"
 
 
 class MistralAIChat:
-    
+
     MODELS = [
-    "mistral-small-latest",
-    "mistral-large-latest",
-    "mistral-moderation-latest",
-    "pixtral-12b-2409",
-]
+        "mistral-small-latest",
+        "mistral-large-latest",
+        "mistral-moderation-latest",
+        "pixtral-12b-2409",
+    ]
 
     def __init__(self, api_key: str, model: str, system_role: str):
         self.api_key = api_key
@@ -41,14 +42,16 @@ class MistralAIChat:
 
     def __validate_model(self, model: str):
         if model not in self.MODELS:
-            raise ValueError(f"Некорректная модель: {model}. Доступные модели: {self.MODELS}")
-        
+            raise ValueError(
+                f"Некорректная модель: {model}. Доступные модели: {self.MODELS}"
+            )
+
         return model
-    
+
     @property
     def model(self):
         return self.__model
-    
+
     @model.setter
     def model(self, model: str):
         self.__model = self.__validate_model(model)
@@ -65,10 +68,108 @@ class MistralAIChat:
         self.__add_message(prompt, "user")
 
         response = self.client.chat.complete(
-            model= self.model,
+            model=self.model,
             messages=self.messages,
         )
 
+        self.__add_message(response.choices[0].message.content, "assistant")
+
+        return response.choices[0].message.content
+
+
+class MistralAiImageChat:
+    MODELS = [
+        "pixtral-12b-2409",
+    ]
+
+    def __init__(self, api_key: str, model: str, system_role: str):
+        self.api_key = api_key
+        self.__model = self.__validate_model(model)
+        self.system_role = system_role
+        self.client = Mistral(api_key=self.api_key)
+
+        self.messages = [
+            {
+                "role": "system",
+                "content": self.system_role,
+            }
+        ]
+
+    def __validate_model(self, model: str):
+        if model not in self.MODELS:
+            raise ValueError(
+                f"Некорректная модель: {model}. Доступные модели: {self.MODELS}"
+            )
+
+        return model
+
+    @property
+    def model(self):
+        return self.__model
+
+    @model.setter
+    def model(self, model: str):
+        self.__model = self.__validate_model(model)
+
+    def __add_message(self, message: str, role: str):
+        self.messages.append(
+            {
+                "role": role,
+                "content": message,
+            }
+        )
+    def __add_image_message(self, message: str, image_base64: str | None = None):
+        new_message = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": message},
+                ]
+            }
+        ]
+
+        if image_base64:
+            new_message[0]["content"].append(
+                {
+                    "type": "image_url",
+                    "image_url": f"data:image/jpeg;base64,{image_base64}",
+                }
+            )
+
+        self.messages.extend(new_message)
+    def __encode_image(self, image_path: str) -> str | None:
+        """Метод кодирования изображений в Base64"""
+
+        try:
+            with open(image_path, "rb") as image_file:
+                return base64.b64encode(image_file.read()).decode("utf-8")
+        except FileNotFoundError:
+            print(f"Ошибка: Файл {image_path} не найден.")
+            return None
+        except Exception as e:
+            print(f"Иная ошибка: {e}")
+            return None
+        
+    def image_text_completion(self, prompt: str, image_path: str | None = None):
+        """
+        Метод для генерации текста по изображению.
+        :param prompt: Текст запроса.
+        :param image_path: Путь к изображению.
+        :return: Сгенерированный текст.
+        """
+        if image_path:
+            image_base64 = self.__encode_image(image_path)
+            if image_base64:
+                self.__add_image_message(prompt, image_base64)
+            else:
+                return "Ошибка при кодировании изображения"
+        else:
+            self.__add_message(prompt, "user")
+
+        response = self.client.chat.complete(
+            model=self.model,
+            messages=self.messages,
+        )
 
         self.__add_message(response.choices[0].message.content, "assistant")
 
@@ -76,12 +177,13 @@ class MistralAIChat:
 
 
 # Тестовый запуск
-chat = MistralAIChat(api_key=MISTRAL_API_KEY, model=model, system_role="Ты эксперт по хокку и поэт. Отвечаешь только в этом стиле")
+image = r"C:\Users\user\Pictures\2025-01-18_14-07-29.png"
+chat = MistralAiImageChat(api_key=MISTRAL_API_KEY, model="pixtral-12b-2409", system_role="Ты опытный разработчик")
+print(chat.image_text_completion("Дай детальную обратную связь по изображению. Там есть текст на русском языке", image))
 
+# Цикл дополнительных вопросов
 while True:
-    prompt = input("Введите ваш запрос: ")
-    if prompt.lower() == "exit":
+    question = input("Задайте вопрос (или напишите 'выход' для завершения): ")
+    if question.lower() == "выход":
         break
-    response = chat.text_completion(prompt)
-    print(response)
-    print("-" * 40)
+    print(chat.image_text_completion(question))
