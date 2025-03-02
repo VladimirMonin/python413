66b8f0ede7630c68c916c2e8e42e5cfaf0366312
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-03-02 15:50:58 +0500

    lesson_25: адаптирвоали класс MistralAIChat для работы с переменными окружения

--- lesson_25.py
+++ lesson_25.py
@@ -4,6 +4,7 @@
 """
 
 from dotenv import load_dotenv
+# pip install python-dotenv
 import os
 
 load_dotenv()
@@ -11,3 +12,53 @@ load_dotenv()
 MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
 
 # Тут мы можем вылавливать из переменных окружения ключи к API
+
+from mistralai import Mistral
+model = "mistral-large-latest"
+
+
+class MistralAIChat:
+    
+    MODELS = [
+    "mistral-small-latest",
+    "mistral-large-latest",
+    "mistral-moderation-latest",
+    "pixtral-12b-2409",
+]
+
+    def __init__(self, api_key: str, model: str, system_role: str):
+        self.api_key = api_key
+        self.__model = self.__validate_model(model)
+        self.system_role = system_role
+        self.client = Mistral(api_key=self.api_key)
+
+    def __validate_model(self, model: str):
+        if model not in self.MODELS:
+            raise ValueError(f"Некорректная модель: {model}. Доступные модели: {self.MODELS}")
+        
+        return model
+    
+    @property
+    def model(self):
+        return self.__model
+    
+    @model.setter
+    def model(self, model: str):
+        self.__model = self.__validate_model(model)
+
+    def text_completion(self, prompt: str):
+        response = self.client.chat.complete(
+            model= self.model,
+            messages = [
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ]
+        )
+        return response.choices[0].message.content
+
+
+# Тестовый запуск
+chat = MistralAIChat(api_key=MISTRAL_API_KEY, model=model, system_role="Ты банан")
+print(chat.text_completion("Напиши басню в стиле Крылова, русского писателя, про мартышку и итератор"))
