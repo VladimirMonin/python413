7f07212ba2cc6009109974208e5bd60cff64afb6
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-03-15 17:08:59 +0500

    lesson_26: знакомство с Абстрактным классом

--- lesson_26.py
+++ lesson_26.py
@@ -14,67 +14,34 @@ Lesson 26 - Наследование
 - MRO - Method Resolution Order
 - Type vs Isinstance
 - Абстрактные классы и методы
+- Модуль ABC - Abstract Base Classes
+- Декоратор @abstractmethod
 """
 
-# 1. Концепция наследования
-# Наследование - это механизм, который позволяет создать новый класс на основе уже существующего.
+from abc import ABC, abstractmethod
 
-class Animal:
-    def __init__(self, name: str):
-        self.name = name
+class AbstractDocument(ABC):
+    def __init__(self, file_path:str):
+        self.file_path = file_path
 
-    def voice(self):
-      return f'{self.__class__.__name__} по имени {self.name} издал(а) звук'
-    
+    @abstractmethod
+    def read(self):
+        pass
+
+    @abstractmethod
+    def write(self):
+        pass
+
+    @abstractmethod
+    def append(self):
+        pass
+
     def __str__(self):
-        return f'{self.__class__.__name__} по имени {self.name}'
-
-class Dog(Animal):
-    # Пайтон ищет метод у собственного класса.
-    # Тут мы переопределили метод voice() у класса Dog.
-    # Теперь будет вызываться метод voice() у класса Dog а не у класса Animal.
-    def voice(self):
-        # Получим результат метода voice() у родительского класса через прямое обращение к нему.
-        # result = Animal.voice(self)
-        # super() - встроеенная функция, которая найдет родительский класс
-        result = super().voice()
-        return f'{result} и это было сделано собакой'
-
-class Cat(Animal):
-    def __init__(self, name: str, fluffy_level: int):
-        super().__init__(name)
-        self.fluffy_level = self.__fluffy_validator(fluffy_level)
-
-    def voice(self):
-        return f'{super().voice()} и это было сделано кошкой'
-
+        return f"Документ типа {self.__class__.__name__} по пути {self.file_path}"
     
-    def __fluffy_validator(self, fluffy_level):
-        if not 0 <= fluffy_level <= 10:
-            raise ValueError('Fluffy level must be between 0 and 10')
-        else:
-            return fluffy_level
 
-dog = Dog('Белка')
-cat = Cat('Хитролап', 5)
+class TextDocument(AbstractDocument):
+    pass
 
-print(dog.voice())  # Dog издало звук. И это было сделано собакой
-print(cat.voice())  # Cat издало звук
-
-# MRO - Method Resolution Order - Порядок разрешения методов
-# Порядок разрешения методов - это порядок, в котором Python ищет методы в иерархии наследования.
-
-# Получим это для Dog
-print(Dog.__mro__)  # (<class '__main__.Dog'>, <class '__main__.Animal'>, <class 'object'>)
-
-# Type vs Isinstance
-# Type - проверяет тип объекта
-# Isinstance - проверяет принадлежность объекта к классу
-
-print(type(dog))  # <class '__main__.Dog'>
-print(isinstance(dog, Dog))  # True
-print(isinstance(dog, Animal))  # True
-
-# <class '__main__.Dog'>
-# True
-# True
+# instantiate abstract class TextDocument without an implementation for abstract metho
+td = TextDocument("text.txt")
