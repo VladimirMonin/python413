1d9172b65581a696fd1e703646e5ed95d5ed18d8
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-03-15 15:55:53 +0500

    lesson_26: переопределение метода родителя

--- lesson_26.py
+++ lesson_26.py
@@ -12,17 +12,24 @@ Lesson 26 - Наследование
 # Наследование - это механизм, который позволяет создать новый класс на основе уже существующего.
 
 class Animal:
+    def __init__(self, name: str):
+        self.name = name
+
     def voice(self):
-        print(f'{self.__class__.__name__} издало звук')
+        print(f'{self.__class__.__name__} по имени {self.name} издал(а) звук')
 
 class Dog(Animal):
-    pass
+    # Пайтон ищет метод у собственного класса.
+    # Тут мы переопределили метод voice() у класса Dog.
+    # Теперь будет вызываться метод voice() у класса Dog а не у класса Animal.
+    def voice(self):
+        print(f'{self.__class__.__name__} по имени {self.name} начал(а) лаять')
 
 class Cat(Animal):
     pass
 
-dog = Dog()
-cat = Cat()
+dog = Dog('Барабос')
+cat = Cat('Святомур')
 
 dog.voice()  # Dog издало звук
 cat.voice()  # Cat издало звук
