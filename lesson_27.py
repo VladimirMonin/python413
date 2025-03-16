49800df366198f782dd639ff20c93270279652e9
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-03-16 16:31:17 +0500

    lesson_27: каноничный правильный вариант использования Mixins без инициализатора

--- lesson_27.py
+++ lesson_27.py
@@ -2,56 +2,34 @@
 Тема: ООП Ч6. Наследование. Миксины. Практика Урок: 27
 """
 
-# Абстрактный класс выпечки
-class Bakery:
-    def __init__(self, **kwargs):
-        self.size = kwargs.pop('size', 30)
-        super().__init__(**kwargs)
+class Pizza:
+    def __init__(self, size: int):
+        self.size = size
 
-# 1. Базовый класс Pizza
-
-
-class Pizza(Bakery):
-    def __init__(self, **kwargs):
-        self.size = kwargs.pop('size', 30)
-        super().__init__(**kwargs)
-
-class Pie(Bakery):
-    def __init__(self, **kwargs):
-        self.size = kwargs.pop('size', 30)
-        super().__init__(**kwargs)
+class Pie:
+    def __init__(self, size: int):
+        self.size = size
 
 
 class CheeseBorderMixin:
-    def __init__(self, **kwargs):
-        self.height = kwargs.pop('height', 10)
-        super().__init__(**kwargs)
-
-    def add_cheese_border(self):
-        print(f'Сырный борт, высотой {self.height} мм активирован!')
+    def add_cheese_border(self, height: int = 10):
+        self.height = height
+        result = f'Сырный борт высотой {self.height} мм активирован!'
+        print(result)
+        return result
 
 class ThinkCrustMixin:
-    def __init__(self, **kwargs):
-        self.thickness = kwargs.pop('thickness', 1)
-    
-    def add_thin_crust(self):
+    def add_thin_crust(self, thickness: int = 1):
+        self.thickness = thickness
         print(f'Тонкое тесто, толщиной {self.thickness} мм активировано!')
 
 
 ######################
 # Пицца с сырным бортом
 class CheeseBorderPizza(Pizza, CheeseBorderMixin):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.add_cheese_border()
+    def __init__(self, size: int, height: int = 10):
+        super().__init__(size)
+        self.add_cheese_border(height)
 
-
-# Тест
-print('Пицца с сырным бортом')
-pizza = CheeseBorderPizza(size=40, height=20)
-print(pizza.size)
-print(pizza.height)
-print(pizza.__dict__)
-print(CheeseBorderPizza.__mro__)
-print('=====================')
-(<class '__main__.CheeseBorderPizza'>, <class '__main__.Pizza'>, <class '__main__.Bakery'>, <class '__main__.CheeseBorderMixin'>, <class 'object'>)
+cbp = CheeseBorderPizza(30)
+print(cbp.size)
