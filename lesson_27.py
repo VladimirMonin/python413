ed2808aca68545a28a94a30fd5985ddc321e2f64
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-03-16 15:36:58 +0500

    lesson_27: Миксины с инициализаторами - плохой вариант

--- lesson_27.py
+++ lesson_27.py
@@ -14,12 +14,17 @@ class Pie:
 
 
 class CheeseBorderMixin:
+    def __init__(self, height: int):
+        self.height = height
+
     def add_cheese_border(self):
-        print("Сырный борт активирован!")
+        print(f'Сырный борт, высотой {self.height} мм активирован!')
 
 class ThinkCrustMixin:
+    def __init__(self, thickness: int):
+        self.thickness = thickness
     def add_thin_crust(self):
-        print("Тонкое тесто активировано!")
+        print(f'Тонкое тесто, толщиной {self.thickness} мм активировано!')
 
 
 ######################
@@ -29,17 +34,6 @@ class ThinkCrustMixin:
 # 2. Нам нужна "мутация" - пицца с сырным бортом. Делаем экземпляр класса PizzaCheeseBorder
 
 class PizzaCheeseBorder(Pizza, CheeseBorderMixin):
-    def __init__(self, size: int):
-        super().__init__(size)
-        self.add_cheese_border()
-
-# 3. Пирог с сырным бортом и тонким тестом
-class PieCheeseBorderThinCrust(Pie, CheeseBorderMixin, ThinkCrustMixin):
-    def __init__(self, size: int):
-        super().__init__(size)
-
-
-p = PieCheeseBorderThinCrust(30)
-print(p.size)
-p.add_cheese_border()
-p.add_thin_crust()
+    def __init__(self, size: int, height: int):
+        Pizza.__init__(self, size)
+        CheeseBorderMixin.__init__(self, height)
